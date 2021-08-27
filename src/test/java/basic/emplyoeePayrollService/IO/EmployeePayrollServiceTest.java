b98887a9db2fe492bd016b2ecba4a18c253e3380
moshtab <mdmohsin41439@gmail.com>
2021-08-27 23:19:09 +0530

    Added print method for displaying the empdata

--- src/test/java/basic/emplyoeePayrollService/IO/EmployeePayrollServiceTest.java
+++ src/test/java/basic/emplyoeePayrollService/IO/EmployeePayrollServiceTest.java
@@ -9,14 +9,15 @@ import basic.emplyoeePayrollService.IO.EmployeePayrollService.IOService;
 public class EmployeePayrollServiceTest {
 	@Test
 	public void given3EmployeeDetailsWhenWrittenToFileShouldMatchEmployeeEntries() {
-		EmployeePayrollData[] arrOfEmps = {
-				new EmployeePayrollData(1,"Mohsin",35000),
-				new EmployeePayrollData(1,"Mohsin",35000),
-				new EmployeePayrollData(1,"Mohsin",35000)
-				
+		EmployeePayrollData[] arrOfEmps = { 
+				new EmployeePayrollData(1, "Mohsin", 35000),
+                new EmployeePayrollData(2, "Sumer", 35000), 
+				new EmployeePayrollData(3, "Sofyan", 35000)
+
 		};
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrOfEmps));
 		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		employeePayrollService.printData(IOService.FILE_IO);
 	}
 
 }
