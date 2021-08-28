04751ea9baf4010b353891485ee60bc6437f39b6
moshtab <87015799+moshtab@users.noreply.github.com>
2021-08-28 21:05:19 +0530

    Add files via upload

---
+++ src/main/java/basic/emplyoeePayrollService/EmployeePayrollFileIOService.java
@@ -0,0 +1,55 @@
+package basic.emplyoeePayrollService.IO;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeePayrollFileIOService {
+	public String payrollFileName = "payroll-file.txt";
+
+	public void writeData(List<EmployeePayrollData> employeePayrollList) {
+		StringBuffer empBuffer = new StringBuffer();
+		employeePayrollList.forEach(employee -> {
+			String empDataString = employee.toString().concat("\n");
+			empBuffer.append(empDataString);
+		});
+		try {
+			Files.write(Paths.get(payrollFileName), empBuffer.toString().getBytes());
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
+
+	}
+
+	public void printData() {
+		try {
+			Files.lines(new File(payrollFileName).toPath()).forEach(System.out::println);
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
+	}
+
+	public long countEntries() {
+		long entries = 0;
+		try {
+			entries = Files.lines(new File(payrollFileName).toPath()).count();
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
+		return entries;
+	}
+
+	public List<EmployeePayrollData> readData() {
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		try {
+			Files.lines(new File(payrollFileName).toPath()).map(line -> line.trim()).forEach(System.out::println);
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
+		return employeePayrollList;
+	}
+
+}
