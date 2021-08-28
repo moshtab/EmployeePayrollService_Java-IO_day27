4d87344e5d8ee97c885e4dd792da3c805e3ddd79
moshtab <mdmohsin41439@gmail.com>
2021-08-28 21:03:36 +0530

    Added read method

--- src/test/java/basic/emplyoeePayrollService/IO/EmployeePayrollServiceTest.java
+++ src/test/java/basic/emplyoeePayrollService/IO/EmployeePayrollServiceTest.java
@@ -11,17 +11,24 @@ import basic.emplyoeePayrollService.IO.EmployeePayrollService.IOService;
 public class EmployeePayrollServiceTest {
 	@Test
 	public void given3EmployeeDetailsWhenWrittenToFileShouldMatchEmployeeEntries() {
-		EmployeePayrollData[] arrOfEmps = { 
-				new EmployeePayrollData(1, "Mohsin", 35000),
-                new EmployeePayrollData(2, "Sumer", 35000), 
-				new EmployeePayrollData(3, "Sofyan", 35000)
+		EmployeePayrollData[] arrOfEmps = { new EmployeePayrollData(1, "Mohsin", 35000),
+				new EmployeePayrollData(2, "Sumer", 35000), new EmployeePayrollData(3, "Sofyan", 35000)
 
 		};
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrOfEmps));
 		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
 		employeePayrollService.printData(IOService.FILE_IO);
 		long entries = employeePayrollService.countEntries(IOService.FILE_IO);
-		assertEquals(3,entries);
+		assertEquals(3, entries);
+
+	}
+
+	@Test
+	public void given3EmployeeDetailsWhenReadingToFileShouldMatchEmployeeCounts() {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		long entries = employeePayrollService.readData(IOService.FILE_IO);
+		assertEquals(6, entries);
+
 	}
 
 }
